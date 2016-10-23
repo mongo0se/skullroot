9c925cfec12b6e05f83ad25e28141e8b60f32025
Mongo0se <mongo0se@users.noreply.github.com>
2016-10-23 13:32:04 +0100

    Update controller.php
    Tidying code and adding comments.

--- core/controller.php
+++ core/controller.php
@@ -1,9 +1,11 @@
 <?php
-
+/**
+ * Core Controller Class
+ */
 class Controller extends Application {
 	
     protected $controller;
-   	protected $action;
+	protected $action;
 	protected $models;
 	protected $view;
 	
@@ -15,7 +17,10 @@ class Controller extends Application {
 		$this->view = new View();
 	}
 	
-	// load model class
+	/**
+	 * Load model
+	 * @param String @model
+	 */
 	protected function load_model($model) {
 
 		if(class_exists($model)) {
@@ -23,36 +28,55 @@ class Controller extends Application {
 		}		
 	}
 	
-	// get instance of the loaded model class
+	/**
+	 * Get Model
+	 * Return an instance of the loaded model class.
+	 * @param String $model
+	 * @return Model|Bool
+	 */
 	protected function get_model($model) {
+		
 		if(is_object($this->models[$model])) {
 			return $this->models[$model];
-		} else {
-			return false;
 		}
+		
+		return false;
 	}
 	
-	// return view object
+	/**
+	 * Get View
+	 * Return view object
+	 * @return View
+	 */
 	protected function get_view() {
 		return $this->view;
 	}
 
-	// return post variables
+	/**
+	 * Get Post
+	 * Return variables from POST superglobal, after sanitising them.
+	 * @return Array
+	 */
 	protected function get_post() {
 
+		// create an empty array
 		$sanitised = array();
-
-		// sanitise post variables
-
+	
+		// if POST is empty
 		if (!$_POST) {
+			
+			// return empty array
 			return $sanitised;
 		}
 
+		// for each value in POST
 		foreach ($_POST as $key => $value) {
 
+			// strip slashes and add to sanitised results
 			$sanitised[$key] = stripslashes($value);
 		}
 
+		// return array
 		return $sanitised;
 	}
 }
